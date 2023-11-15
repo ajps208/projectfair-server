228b77a1bbc903e721a55d380d8ffc67bb476804
ajps208 <ajithps208@gmail.com>
2023-11-15 11:33:04 +0530

    changed

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,5 +1,7 @@
-// register
 const users=require('../Models/userSchema')
+const jwt=require('jsonwebtoken')
+
+// register
 exports.register=async(req,res)=>{
     console.log("inside register controller function");
     const {username,email,password}=req.body
@@ -18,5 +20,25 @@ exports.register=async(req,res)=>{
     catch(err){
         res.status(200).json(`Register Api failed, Error:${err}`)
 
+    }
+}
+
+// login
+exports.login=async(req,res)=>{
+    console.log("inside login function");
+    const{email,password}=req.body
+    try{
+        const existingUser=await users.findOne({email,password})
+        if(existingUser){
+            const token=jwt.sign({userId:existingUser._id},"supersecretkey12345")
+            res.status(200).json({
+                existingUser,token
+            })
+        }else{
+            res.status(404).json('incorrect Email / Password')
+        }
+    }
+    catch(err){ 
+        res.status(401).json(`login api failed : ${err}`)
     }
 }
--- Routes/router.js
+++ Routes/router.js
@@ -3,6 +3,7 @@ const router=new express.Router()
 const userController=require('../Controller/userController')
 // register
 router.post('/user/register',userController.register)
-
+// login
+router.post('/user/login',userController.login)
 // export router
 module.exports=router
